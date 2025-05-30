4db5cfcb91ae66c256a2a247c2ae0be03e0240bf
Alan Marinho <a13br1313@gmail.com>
2025-05-30 13:36:52 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://games.alanmarinho.com.br',
     credentials: true,
   }),
 );
