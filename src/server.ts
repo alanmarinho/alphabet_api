c168a94aac54e637d552b732968e04f2d5d98c68
Alan Marinho <a13br1313@gmail.com>
2025-05-30 15:53:38 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ const port = process.env.PORT || 3000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
+app.set('trust proxy', 1);
 app.use(
   cors({
     origin: 'https://games.alanmarinho.com.br',
